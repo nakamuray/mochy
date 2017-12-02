822a1721b42977aec72596cdb991187ed6188575
NAKAMURA Yoshitaka <arumakanoy@gmail.com>
2017-12-02 17:35:45 +0900

    define response using lua script

--- README.md
+++ README.md
@@ -2,21 +2,41 @@
 
 `mochy` is a mock HTTP server.
 
-It just do these things:
+It do these things:
 
-* return HTTP response specified by command-line argument
+* return HTTP response specified by command-line argument (lua script)
 * dump HTTP request it receive to terminal
 
 ```
-Usage of mochy:
+Usage: mochy [options] <lua script>
   -addr string
         address to serve (default ":8080")
-  -body string
-        response body (default "It's Works!\n")
-  -code int
-        status code (default 200)
-  -content-type string
-         (default "text/plain; charset=utf-8")
-  -file string
-        file path to use as a response body
+  -f string
+        script filename
+```
+
+lua script is expeted to return:
+* string, which is used as a response body
+* table with key "code", "contentType" and "body"
+* function, which return those things. this function is called at each request
+
+## examples
+return fixed string:
+```
+$ mochy 'return "hello world\n"'
+```
+
+return 404 response:
+```
+$ mochy 'return {code=404, body="sorry.\n"}'
+```
+
+return random numbers:
+```
+$ mochy 'return function() return math.random() .. "\n" end'
+```
+
+return back requrest path name:
+```
+$ mochy 'return function(req) return req.url.path .. "\n" end'
 ```
--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/yuin/gopher-lua"
 	"io"
+	luajson "layeh.com/gopher-json"
 	"log"
 	"net/http"
 	"os"
@@ -12,19 +14,35 @@ import (
 )
 
 type Mock struct {
-	statusCode  int
-	contentType string
-	body        string
-	filepath    string
+	lua    *lua.LState
+	lvalue lua.LValue
 }
+
+const DEFAULT_BODY = "It's Works!\n"
+const DEFAULT_CODE = 200
+const DEFAULT_CONTENT_TYPE = "text/plain; charset=utf-8"
 
 func (m *Mock) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	dumpRequest(r)
-	var body io.Reader
 
-	if m.filepath != "" {
-		file, err := os.Open(m.filepath)
-		if err != nil {
+	var lv lua.LValue
+	if f, ok := m.lvalue.(*lua.LFunction); ok {
+		reqTable := m.lua.NewTable()
+		m.lua.SetTable(reqTable, lua.LString("method"), lua.LString(r.Method))
+
+		urlTable := m.lua.NewTable()
+		m.lua.SetTable(urlTable, lua.LString("hostname"), lua.LString(r.URL.Hostname()))
+		// TODO: convert port to int
+		m.lua.SetTable(urlTable, lua.LString("port"), lua.LString(r.URL.Port()))
+		m.lua.SetTable(urlTable, lua.LString("path"), lua.LString(r.URL.Path))
+		m.lua.SetTable(urlTable, lua.LString("querystring"), lua.LString(r.URL.RawQuery))
+		m.lua.SetTable(reqTable, lua.LString("url"), urlTable)
+		// TODO: put more infomations into reqTable
+		if err := m.lua.CallByParam(lua.P{
+			Fn:      f,
+			NRet:    1,
+			Protect: true,
+		}, reqTable); err != nil {
 			log.Print(err)
 
 			w.WriteHeader(http.StatusInternalServerError)
@@ -32,13 +50,39 @@ func (m *Mock) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 			return
 		}
-		body = file
+		lv = m.lua.Get(-1) // returned value
+		m.lua.Pop(1)       // remove recived value
 	} else {
-		body = strings.NewReader(m.body)
+		lv = m.lvalue
 	}
 
-	w.Header().Set("Content-Type", m.contentType)
-	w.WriteHeader(m.statusCode)
+	body := strings.NewReader(DEFAULT_BODY)
+
+	if table, ok := lv.(*lua.LTable); ok {
+		code := DEFAULT_CODE
+		contentType := DEFAULT_CONTENT_TYPE
+
+		m.lua.ForEach(table, func(key, value lua.LValue) {
+			switch key.String() {
+			case "code":
+				if lcode, ok := value.(lua.LNumber); ok {
+					code = int(lcode)
+				} else {
+					log.Printf("not a number: %s", value)
+				}
+			case "contentType":
+				contentType = value.String()
+			case "body":
+				body = strings.NewReader(value.String())
+			default:
+				log.Printf("unknown key: %s\n", key)
+			}
+		})
+		w.Header().Set("Content-Type", contentType)
+		w.WriteHeader(code)
+	} else {
+		body = strings.NewReader(lv.String())
+	}
 
 	if _, err := io.Copy(w, body); err != nil {
 		log.Print(err)
@@ -63,28 +107,50 @@ func dumpRequest(r *http.Request) {
 	fmt.Fprintf(w, "\n")
 }
 
-var statusCode int
-var contentType string
-var body string
-var filepath string
 var addr string
+var scriptfile string
 
 func init() {
-	flag.IntVar(&statusCode, "code", 200, "status code")
-	flag.StringVar(&contentType, "content-type", "text/plain; charset=utf-8", "")
-	flag.StringVar(&body, "body", "It's Works!\n", "response body")
-	flag.StringVar(&filepath, "file", "", "file path to use as a response body")
 	flag.StringVar(&addr, "addr", ":8080", "address to serve")
+	flag.StringVar(&scriptfile, "f", "", "script filename")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [options] <lua script>\n", os.Args[0])
+		flag.PrintDefaults()
+	}
 }
 
 func main() {
 	flag.Parse()
 
+	L := lua.NewState()
+	defer L.Close()
+
+	luajson.Preload(L)
+	if err := L.DoString(`json = require('json')`); err != nil {
+		log.Fatal(err)
+	}
+
+	var lvalue lua.LValue
+
+	if scriptfile != "" {
+		if err := L.DoFile(scriptfile); err != nil {
+			log.Fatal(err)
+		}
+		lvalue = L.Get(-1)
+	} else if flag.NArg() > 0 {
+		code := flag.Arg(0)
+		if err := L.DoString(code); err != nil {
+			log.Fatal(err)
+		}
+		lvalue = L.Get(-1)
+	} else {
+		// TODO: load code from file or stdin
+		lvalue = lua.LString("It's Works!\n")
+	}
+
 	m := Mock{
-		statusCode:  statusCode,
-		contentType: contentType,
-		body:        body,
-		filepath:    filepath,
+		lua:    L,
+		lvalue: lvalue,
 	}
 
 	log.Printf("serve at %s\n", addr)
